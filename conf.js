85d2af8397049503e427568a0d35b229612f3a7a
Apollo Tang <tang.apollo@gmail.com>
2015-12-26 16:29:25 -0500

    mv statsSpec into configuration file

--- conf.js
+++ conf.js
@@ -12,4 +12,16 @@ c.SCHEMA = {
     'os' : ''  // os
 };
 
+c.statsSpec = {
+    ge: {
+        split: [0, 0.1, 0.2, 1],
+        splitGroup:['M', 'F', 'D'],
+        dimension: 'ge'
+    },
+    ct: {
+        split: [0, 0.1, 0.2, 0.3, 1],
+        splitGroup: ['T', 'M', 'V', 'D'],
+        dimension: 'ct'
+    },
+}
 module.exports = c;
--- dataGenerator.js
+++ dataGenerator.js
@@ -14,7 +14,11 @@ function startNewSet(howMany, ff, rj) {
     ff(JSON.stringify(dataSet));
 }
 
-function cb(datum, split, splitGroup, dataProps) {
+function cb(datum, opts) {
+    const split      = opts.split;
+    const splitGroup = opts.splitGroup;
+    const dimension  = opts.dimension;
+
     const spec = [];
     for (var i=1; i <= split.length-1; i++) {
         spec.push( [ split[i-1], split[i], splitGroup[i-1] ]);
@@ -25,7 +29,7 @@ function cb(datum, split, splitGroup, dataProps) {
 
     _(spec).each(function(i){
         if (( N*i[0] < rand ) && ( rand <= N*i[1])) {
-            datum[dataProps] = i[2];
+            datum[dimension] = i[2];
         }
     }).value();
 
@@ -34,62 +38,13 @@ function cb(datum, split, splitGroup, dataProps) {
 
 function generateStats(data, ff, rj) {
     let data_new = {};
-    // const N = 10000;
-    // let rand;
-
     data_new = _(data)
     .map(function(datum){
-       return cb(datum,
-            [0, 0.1, 0.2, 1],
-            ['M', 'F', 'D'],
-            'ge'
-        );
+        return cb(datum, cf.statsSpec.ge);
     })
     .map(function(datum){
-       return cb(datum,
-             [0, 0.1, 0.2, 0.3, 1],
-             ['T', 'M', 'V', 'D'],
-             'ct'
-       );
+        return cb(datum, cf.statsSpec.ct);
     }).value();
-
-    // .map(function(datum, j){
-    //     const split      = [0, 0.1, 0.2, 1];
-    //     const splitGroup = ['M', 'F', 'D'];
-    //
-    //     const spec = [];
-    //     for (var i=1; i <= split.length-1; i++) {
-    //         spec.push( [ split[i-1], split[i], splitGroup[i-1] ]);
-    //     }
-    //
-    //     rand = Math.floor(Math.random()*N + 1);
-    //     _(spec).each(function(i){
-    //         if (( N*i[0] < rand ) && ( rand <= N*i[1])) {
-    //            datum.ge = i[2];
-    //         }
-    //     }).value();
-    //
-    //     return datum;
-    // })
-    // .map(function(datum, j){
-    //     const split      = [0, 0.1, 0.2, 0.3, 1];
-    //     const splitGroup = ['T', 'M', 'V', 'D'];
-    //
-    //     const spec = [];
-    //     for (var i=1; i <= split.length-1; i++) {
-    //         spec.push( [ split[i-1], split[i], splitGroup[i-1] ]);
-    //     }
-    //
-    //     rand = Math.floor(Math.random()*N + 1);
-    //     _(spec).each(function(i){
-    //         if (( N*i[0] < rand ) && ( rand <= N*i[1])) {
-    //            datum.ct = i[2];
-    //         }
-    //     }).value();
-    //
-    //     return datum;
-    // });
-    // ;
 
     ff(JSON.stringify(data_new));
 }
