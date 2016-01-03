56ef113d6fe194f0f861c28071ff7e0b56b9e463
Apollo Tang <tang.apollo@gmail.com>
2016-01-03 14:54:41 -0500

    summary will analyse data to tabulate all distinct value

--- summary.js
+++ summary.js
@@ -8,33 +8,61 @@ const cf = require('./conf');
 const dataFile = cf.dataFile;
 
 
+let _distinctValueMap = {}; //{ ge: [M, F, D],  ...dimension: distinctValues.. .}
+
 module.exports = function summarizeDataInFile(fileName) {
-    // @TODO open file
     fs.readFileAsync(dataFile)
-        .then( function(b){ generateSummary( JSON.parse(b.toString()) )
+        .then( parseToObject )
+        .then( analysisData )
+        .then( generateSummary )
         .then( console.log )
-    });
-    // @TODO close file
 };
+
+function parseToObject(buffer){
+    return JSON.parse(buffer.toString());
+}
+
+function analysisData(data) {
+    return new Promise(function(ff, rj) {
+        console.log('Analysing data...');
+        const summary = 'Data length: ' + data.length + '\n';
+
+        // dimensions = ['dd', 'ct','ge','ag', 'os'];
+        const dimensions = _(Object.keys(cf.SCHEMA)).filter(function(i){
+            if ( i !== 'id' ) return true; // remove 'id'
+            return false;
+        }).value();
+
+        // Build disctinct value map
+        dimensions.forEach(function(dimension){
+            _distinctValueMap[dimension] = _.uniq(_(data).reduce(function(acc, datum){
+                if (datum[dimension]) {  // ingore empty string
+                    acc.push(datum[dimension])
+                };
+                return acc;
+            }, []));
+        });
+
+        console.log('Done analysing data: disctinct value map built');
+        ff(data);
+    });
+}
 
 function generateSummary(data) {
     return new Promise(function(ff, rj){
+        console.log('Generating summary...');
         let summary = '';
         summary += 'Data length: ' + data.length + '\n';
-        Object.keys(cf.statsSpecs).forEach(function(key){
-            let i = cf.statsSpecs[key];
-            const dimension = key;
-            const splitGroup = i.splitGroup
-            summary += 'dimension: ' + i.dimension + '\n';
-            _(splitGroup).each(function(group){
-                let filter = {};
-                filter[dimension] = group;
-                const filtered = _( data ).filter(filter).value()
+        Object.keys(_distinctValueMap).forEach(function(dimension){
+            let distinctValues = _distinctValueMap[dimension];
+            summary += 'Dimension: ' + dimension + '\n';
+            distinctValues.forEach(function(dv){
+                const filtered = _( data ).filter(dimension, dv).value()
                 const length = filtered.length
-                summary += group + ': ' + length + '\n';
-            }).value();
+                summary += dv + ': ' + length + '\n';
+            });
         });
-
+        console.log('Summary generated');
         ff(summary);
     });
 }
